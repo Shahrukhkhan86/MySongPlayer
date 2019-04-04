1970671b89d4b553aaae0ff1d569cea662f0d5b0
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-04-04 11:09:15 +0530

    init commit

--- app/src/main/java/com/example/mysongplayer/MainActivity.java
+++ app/src/main/java/com/example/mysongplayer/MainActivity.java
@@ -9,17 +9,26 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Button Push;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        Push = findViewById(R.id.button2);
         final MediaPlayer mediaPlayer=MediaPlayer.create(MainActivity.this,R.raw.aa);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mediaPlayer.start();
+            }
+        });
+        Push.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+              // mediaPlayer.isPlaying();
             }
         });
     }
