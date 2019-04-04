0cc357edf7f99681b2147c1488e287f6d6f4f24b
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-04-04 10:41:34 +0530

    add button and add ongs

--- app/src/main/java/com/example/mysongplayer/MainActivity.java
+++ app/src/main/java/com/example/mysongplayer/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
     }
 }
