ac4b84429086d35702dcc6d37bce959b605ff6f4
Shahrukhkhan86 <shahrukh.khan_mca18@gla.ac.in>
2019-04-04 10:44:05 +0530

    ad some code in java

--- app/src/main/java/com/example/mysongplayer/MainActivity.java
+++ app/src/main/java/com/example/mysongplayer/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mysongplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        final MediaPlayer mediaPlayer=MediaPlayer.create(MainActivity.this,R.raw.aa);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
     }
 }
